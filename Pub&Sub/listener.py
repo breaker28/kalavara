3ae90b2d4ab6bf1c4b8b7cc53b8be41f6fd37d32
Akshay R <158812930+breaker28@users.noreply.github.com>
2025-10-29 22:13:54 +0530

    Update listener.py

--- Pub&Sub/listener.py
+++ Pub&Sub/listener.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env Python3
+#!/usr/bin/env python3
 #license removed for brevity
 import rospy
 from std_msgs.msg import String
@@ -6,7 +6,7 @@ def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s",data.data)
 def listener():
         rospy.init_node('listener',anonymous=True)
-        rospy.subscriber('chatter,String,callbback)
+        rospy.Subscriber('chatter', String, callback)
         rospy.spin()
 if __name__=='__main__':
     listener()
