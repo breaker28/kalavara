16ed89a5fb5780fa98884566aa4e9686c877a2f9
Akshay R <158812930+breaker28@users.noreply.github.com>
2025-10-29 18:32:20 +0530

    Create ledblink inbuilt.cpp

---
+++ Arduino/ledblink inbuilt.cpp
@@ -0,0 +1,22 @@
+#include<ros.h>
+#include<std_msgs/strings.h>
+ros::NodeHandle nh;
+void message ch(const std_msgs::Empty&toggle_msg)
+{
+digitalWrite(LED_BuiltIn,High);
+delay(3000);
+digitalWrite(LED_BuiltIn,Low);
+delay(3000);
+}
+ros::Subscriber<std_msgs::Empty>sub("Led_blink,"&messagecb);
+void setup()
+{
+  PinMode(LED_BuiltIn,output);
+  nh.initNode();
+  nh.Subscriber(sub);
+}
+void loop()
+{
+nh.spinOnce();
+}
+
